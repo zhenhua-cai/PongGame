f82ea2cca6f498c619a0c46a4764a7c6172ea5ba
zhenhua cai <steven.cai1990@hotmail.com>
2018-07-04 17:09:35 -0400

    Initial Commit

---
+++ GameProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameProject
+//
+//  Created by Zhenhua Cai on 7/4/18.
+//  Copyright © 2018 Zhenhua Cai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
